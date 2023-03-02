76209091a492a1c6b54b2470e81d12f1de8df445
Abdullah149081 <masudmd425@gmail.com>
2023-03-02 22:56:06 +0600

    Card show details

--- js/index.js
+++ js/index.js
@@ -13,24 +13,32 @@ const loadAIShow = (showDAta) => {
 
   showDAta.forEach((data) => {
     const { image, features, name, published_in } = data;
+    const [a, b, c] = features;
+
+    // console.log(features);
 
     const divTag = document.createElement("div");
     divTag.classList.add("col");
     divTag.innerHTML = `
         <div class="card h-100">
-            <img src="${image}" class="card-img-top" alt="..." />
+            <img src="${image}" class="card-img-top p-3 h-50" alt="..." />
             <div class="card-body">
-                <h5 class="card-title">Features:${features}</h5>
-                
-
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <h5 class="card-title">Features :</h5>
+               <ol>
+                <li>${a ? a : "not available"}</li>
+                <li>${b ? a : "not available"}</li>
+                <li>${c ? c : "not available"}</li>
+                </ol>
+                <hr>
+                <p class="card-text fw-bold ">${name}</p>
+                <p class="fw-bold">Date: ${published_in}</p>
             </div>
         </div>
     
-    
     `;
+
     aiContainer.appendChild(divTag);
   });
 
-  console.log(showDAta);
+  //   console.log(showDAta);
 };
