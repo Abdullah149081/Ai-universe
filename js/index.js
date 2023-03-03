a85cbd1079c8be4ed30bd3a67f264b34badff257
Abdullah149081 <masudmd425@gmail.com>
2023-03-03 19:09:08 +0600

    modal details add

--- js/index.js
+++ js/index.js
@@ -43,7 +43,7 @@ const loadAIData = (showData, dataLimit) => {
          <div class="d-flex justify-content-between align-items-center">
             <p class="fw-bold"><i class="fa fa-calendar" aria-hidden="true"></i> ${published_in}</p>
                 <div>
-                <button class="rounded-circle border-0"><i class="fas fa-arrow-right text-danger"></i></button>
+                <button onclick="aiModal('${id}')" class="rounded-circle border-0"><i class="fas fa-arrow-right text-danger" data-bs-toggle="modal" data-bs-target="#AiUniverseModal"></i></button>
                 </div>
         </div>
 
@@ -80,4 +80,52 @@ const showAllData = async () => {
 const spinnerLoad = (isLoading) => {
   const spinner = document.getElementById("spinner");
   isLoading === true ? spinner.classList.remove("d-none") : spinner.classList.add("d-none");
+};
+
+const aiModal = (id) => {
+  const URL = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
+
+  fetch(URL)
+    .then((res) => res.json())
+    .then((data) => modalShow(data.data))
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const modalShow = (data) => {
+  const { description, pricing } = data;
+  const [item, item2, item3] = pricing;
+
+  console.log(item);
+
+  const modalDetails = document.getElementById("modal-details");
+  modalDetails.innerHTML = `
+
+  <div class="container-fluid">
+  <div class="row">                
+    <div class="col-lg-6">
+      <div class="border border-danger border-3 rounded p-4 bg-danger bg-opacity-10">
+        <p class="fw-bold">${description}</p>
+        <!-- 3 div  -->
+        <div>
+          <div class="d-flex gap-3 flex-md-row flex-column">
+            <p class="bg-light p-3 rounded fw-bold text-center  text-success">${item.price ? item.price : "Free of Cost/Basic"} <br />${item.plan ? item.plan : "no"}</p>
+            <p class="bg-light p-3 rounded fw-bold text-center  text-warning">${item2.price} <br />${item2.plan}</p>
+            <p class="bg-light p-3 rounded fw-bold text-center  text-danger">${item3.price} <br />${item3.plan}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-6">.col-md-4 .ms-auto</div>
+  </div>
+</div>
+
+ 
+ 
+ 
+ 
+ 
+ 
+ `;
 };
